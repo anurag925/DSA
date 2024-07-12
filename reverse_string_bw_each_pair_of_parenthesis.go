1f18dc5c09d419e1e353ed0331bdd5d642fefaec
Anurag Upadhyay <anuragle100gmail.com>
2024-07-12 17:13:30 +0530

    daily solve

---
+++ reverse_string_bw_each_pair_of_parenthesis.go
@@ -0,0 +1,23 @@
+package dsa
+
+func reverseParentheses(s string) string {
+	temp := make([]string, 0)
+	stack := make([]string, 0)
+	for _, e := range s {
+		if string(e) == ")" {
+			for len(stack) != 0 && stack[len(stack)-1] != "(" {
+				temp = append(temp, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, temp...)
+			temp = make([]string, 0)
+		} else {
+			stack = append(stack, string(e))
+		}
+	}
+	res := ""
+	for _, e := range stack {
+		res += e
+	}
+	return res
+}
